baf97baf960658c52bf511ac4b2f84b9716ddc63
Alfred <a1868017@adelaide.edu.au>
2022-08-03 12:40:55 +0930

    change

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -4,10 +4,12 @@ using namespace std;
 void print_summed(int array1[3][3],int array2[3][3]){
 for(int i=0;i<3;i++){
     for(int j=0;j<3;j++){
-        printf("%d ",array1[i][j]+array2[i][j]);
         if(j==2){
+           printf("%d",array1[i][j]+array2[i][j]);
            printf("\n"); 
+           break;
         }
+        printf("%d ",array1[i][j]+array2[i][j]);
     }
 }
 }
